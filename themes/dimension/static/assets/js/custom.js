f145634efab325495c4513c4358116f2accf5911
ccthecode <ccthecode@gmail.com>
2022-09-09 21:14:59 +0100

    fix menulist

--- themes/dimension/static/assets/js/custom.js
+++ themes/dimension/static/assets/js/custom.js
@@ -1,3 +1,8 @@
 // auto remove extra border on even links
 const menulist = document.querySelector('#header nav ul')
-menulist.children.length % 2 == 1 ? menulist.children[menulist.children.length-1].style.borderLeft = 0 : false
+const menulistA = document.querySelectorAll('#header nav ul a')
+if (menulist.children.length % 2 == 0) { 
+    menulistA.forEach(menu => {
+        menu.classList.add('even-width')  
+    }); 
+}

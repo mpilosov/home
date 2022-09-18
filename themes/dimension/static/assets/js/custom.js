e4c5b2893cbfffc00405eebd905c316bb90f7cec
Elliott Regan <elliott.regan@hey.com>
2022-09-18 15:40:38 -0600

    remove JS that adjusted size of navigation items

--- themes/dimension/static/assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-// auto remove extra border on even links
-const menulist = document.querySelector('#header nav ul')
-const menulistA = document.querySelectorAll('#header nav ul a')
-if (menulist.children.length % 2 == 0) { 
-    menulistA.forEach(menu => {
-        menu.classList.add('even-width')  
-    }); 
-}
